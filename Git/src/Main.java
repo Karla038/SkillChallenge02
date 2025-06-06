9f6bab377d296096239e96c885fab914f6e238b8
Karla038 <karladk038@gmail.com>
2025-06-05 18:13:44 -0600

    Se agrego la logica para crear personas

--- Git/src/Main.java
+++ Git/src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
+
+    static class Persona{
+        String nombre;
+        String pasatiempo;
+
+        public  Persona( String nombre, String pasatiempo){
+            this.nombre = nombre;
+            this.pasatiempo = pasatiempo;
+        }
+
+        public void presentacion(){
+            System.out.println(" Hola soy " + nombre + " y me gusta mucho " + pasatiempo );
+        }
+
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!"
-        );
+        Persona persona1 = new Persona( "Karla", "leer. ");
+
+        persona1.presentacion();
+
     }
 }
